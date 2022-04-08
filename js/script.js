aa56de6a061e6848d089f010117e0c343aecd3c0
Jasurbek03 <nurillayev.jasurbek0305@gmail.com>
2022-04-08 10:40:15 +0500

    git init .

---
+++ js/script.js
@@ -0,0 +1,22 @@
+alert("Assalomu aleykum. Sizni Saytimizda ko'rib turganimizdan hursandmiz!!!");
+var firstName = prompt("Iltimos Ismingiz kiriting");
+var lastName = prompt("Familiyangiz?");
+var age = prompt("Yoshingiz?");
+var gender = prompt("Jinsingiz?");
+var nation = prompt("Millatingiz nima?");
+var job = prompt("Qayrda Ishlaysiz (yoki o'qiysiz)?");
+
+// console.log("Foydalanuvchi Ismi: " + firstName);
+// console.log("Foydalanuvchi Fmiliyasi: " + lastName);
+// console.log("Foydalanuvchi yoshi: " + age);
+// console.log("Foydalanuvchi jinsi: " + gender);
+// console.log("Foydalanuvchi millati: " + nation);
+// console.log("Foydalanuvchi kasbi: " + job);
+
+console.log(
+  "Foydalanuvchi Ismi: " + firstName + "\n" +
+  "Foydalanuvchi Fmiliyasi: " + lastName + "\n" +
+  "Foydalanuvchi yoshi: " + age + "\n" +
+  "Foydalanuvchi jinsi: " + gender + "\n" +
+  "Foydalanuvchi millati: " + nation + "\n" +
+  "Foydalanuvchi kasbi: " + job);
